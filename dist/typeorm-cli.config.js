d963606ef743296f48e3b7d0bbcb3603602a162f
gr4yx01 <hoyx0101@gmail.com>
2025-06-28 23:40:52 +0200

    fix: build

--- dist/typeorm-cli.config.js
+++ dist/typeorm-cli.config.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const typeorm_1 = require("typeorm");
+const _1751139307305_firstMigration_1 = require("./src/migrations/1751139307305-firstMigration");
 exports.default = new typeorm_1.DataSource({
     type: 'postgres',
     host: 'localhost',
@@ -9,6 +10,6 @@ exports.default = new typeorm_1.DataSource({
     password: 'pyr@hornet0101',
     database: 'nestblog',
     entities: ['**/*.entity.js'],
-    migrations: ['migrations/*.js'],
+    migrations: [_1751139307305_firstMigration_1.FirstMigration1751139307305],
 });
 //# sourceMappingURL=typeorm-cli.config.js.map
